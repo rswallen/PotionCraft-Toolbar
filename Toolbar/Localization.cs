6512b846a6bd65d7cca61a788308e2e7d2b15872
rswallen <rswallen@gmail.com>
2022-12-28 16:53:41 +0000

    Fixed bug caused by incomplete localization

--- Toolbar/Localization.cs
+++ Toolbar/Localization.cs
@@ -2,17 +2,21 @@
 using HarmonyLib;
 using PotionCraft.Assemblies.DataBaseSystem.PreparedObjects;
 using PotionCraft.LocalizationSystem;
+using System;
 
 namespace Toolbar
 {
     internal class Localization
     {
-        static ManualLogSource Log => ToolbarPlugin.Log;
-
         [HarmonyPostfix, HarmonyPatch(typeof(LocalizationManager), "LoadLocalizationData")]
         public static void LoadLocalizationData_Postfix(ref LocalizationData __result)
         {
-            __result.Add((int)LocalizationManager.Locale.en, "toolbar_opentoolbar_tooltip", "Mod Toolbar");
+            // key.GetText throws an error if there isn't text for every language
+            // temp solution unless/until proper localisation can be created
+            foreach (int lang in Enum.GetValues(typeof(LocalizationManager.Locale)))
+            {
+                __result.Add(lang, "toolbar_opentoolbar_tooltip", "Mod Toolbar");
+            }
         }
     }
 }
